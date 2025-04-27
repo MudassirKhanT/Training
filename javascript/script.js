24bc646a24d5b5da4585a87765d59a66aa8f9d11
MudassirKhanT <mudassirkhantailor@gmail.com>
2025-04-27 11:43:08 +0530

    js integration in html

---
+++ javascript/script.js
@@ -0,0 +1,15 @@
+// console.log("Mudassir Khan");
+// name = "Mudassir Khan Tailor";
+// age = 21;
+// is_third_year = true;
+// console.log(name, "Welcome to MITS");
+// //Variables can have digits,_,$
+// //reserved words can't be used
+// // let let= "a";
+// // console.log(let);
+// person1 = "khan";
+// _name = "mudassir";
+// $naam = "mudassirkhan";
+// console.log(person1);
+// console.log(_name);
+// console.log($naam);
