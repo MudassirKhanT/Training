7e6791398faab0c82649352cf6155aa696acfca2
MudassirKhanT <mudassirkhantailor@gmail.com>
2025-04-27 12:54:39 +0530

    grades

--- javascript/conditional.js
+++ javascript/conditional.js
@@ -23,6 +23,31 @@
 // if (age > 3 && age < 5) {
 //   price -= 100;
 //   console.log(`Half Ticket Price:${price}`);
-// } else {
+// } else  {
 //   console.log(`Full Ticket Price:${price}`);
 // }
+
+//--------------Task-4---------
+
+// num = prompt("Enter a num:");
+// if (num % 5 == 0) {
+//   console.log(`${num} is a multiple of 5`);
+// } else {
+//   console.log(`${num} is a not multiple of 5`);
+// }
+
+//--------------Task-5-----------
+let percentage = prompt("Enter Percentage:");
+if (percentage >= 90 && percentage < 100) {
+  console.log("A");
+} else if (percentage >= 80 && percentage < 90) {
+  console.log("B");
+} else if (percentage >= 70 && percentage < 80) {
+  console.log("C");
+} else if (percentage >= 60 && percentage < 70) {
+  console.log("D");
+} else if (percentage >= 50 && percentage < 60) {
+  console.log("E");
+} else {
+  console.log("F");
+}
