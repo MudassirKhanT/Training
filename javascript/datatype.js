55cfc11ac9918b755cba19bbda7fca5997beefc9
MudassirKhanT <mudassirkhantailor@gmail.com>
2025-04-27 11:42:37 +0530

    datatypes in js

---
+++ javascript/datatype.js
@@ -0,0 +1,44 @@
+/***DATA_TYPES:Primitive
+ * string
+ * boolean
+ * number
+ * undefined
+ * null
+ * bigInt
+ * Symbol
+ * -----------------------NON_PREMITIVE---------------
+ * Objects
+ */
+//--------------premitive--------------
+// let name = "mudassir"; //string
+// let age = 20; //number
+// let is_third_year = true; //boolean
+// let required; // undefined
+// let NULL = null; //null
+
+// console.log(required);
+// //-----------------Objects-----------------
+// let cst = {
+//   dept: "Cst",
+//   sections: 3,
+//   hod: "Dinesh",
+//   strenght: 201,
+//   isWorking: false,
+// };
+// //Accessing an object must include string cst['hod']
+// //Performing operations on a object
+// console.log(cst["isWorking"]);
+// cst["strenght"] = cst["strenght"] * 100;
+// console.log(cst["strenght"]);
+
+//------Task1-LinkedIn Object and do modiifcations--------
+
+// let linkedIn = {
+//   name: "Mudassir Khan Tailor",
+//   is_verified: false,
+//   followers: 100,
+//   pno: 6301721009,
+// };
+// linkedIn["followers"] = linkedIn["followers"] + 100000;
+// console.log(linkedIn["followers"]);
+// console.log(linkedIn.is_verified);
