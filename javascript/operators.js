7636a0a003f58d2b8fc79ab4938882b0f5127fde
MudassirKhanT <mudassirkhantailor@gmail.com>
2025-04-27 11:42:56 +0530

    operators in js

---
+++ javascript/operators.js
@@ -0,0 +1,47 @@
+/***
+ * +
+ * -
+ * *
+ * /
+ * %
+ * ++ etc
+ */
+// let num1 = 20;
+
+// let num2 = 30;
+//----------Arithmetic----------
+// console.log(`Addition:${num1 + num2}`);
+// console.log(`Sub:${num1 - num2}`);
+// console.log(`Mul:${num1 * num2}`);
+// console.log(`Div:${num1 / num2}`);
+// console.log(`Rem:${num1 % num2}`);
+// console.log(`power:${num1 ** num2}`);
+
+// num1++;
+// console.log(`Addition:${num1}`);
+// console.log(`AND:${num1 & num2}`);
+// console.log(`OR:${num1 | num2}`);
+// console.log(`XOR:${num1 ^ num2}`);
+
+//-----------uniary-----------------
+// console.log(`num1++ =:${num1++}`);
+// console.log(`++num1 =:${++num1}`);
+// console.log(`num1-- =:${num1--}`);
+// console.log(`--num1 =:${--num1}`);
+
+//----------comparision-------------
+/***
+ * 1.== checks only the value
+ * 2.!=
+ * 3.=== strictly equal (checks both value and type)
+ * 4.!== not strictly equal
+ * 5. = assigns
+ */
+
+let num1 = "25";
+let num2 = 25;
+console.log(typeof num1);
+console.log(`Equals: ${num1 == num2}`);
+console.log(`Not Equals: ${num1 != num2}`);
+console.log(`Strictly Equals: ${num1 === num2}`);
+console.log(`Strictly Not Equals: ${num1 !== num2}`);
