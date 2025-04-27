98fc9801058cd5622e2413f9dba32fd39ddfdf6a
MudassirKhanT <mudassirkhantailor@gmail.com>
2025-04-27 12:33:56 +0530

    learnt conditionals statements and performed task

---
+++ javascript/conditional.js
@@ -0,0 +1,28 @@
+//User input using prompt
+// let user_input = prompt("Enter user name:");
+// let age = prompt("Enter age:");
+// console.log(user_input, "hello hi");
+
+/*** Task-2
+ * Take user name,age
+ * if age is greater than 18 eligible for vote with the user name as output
+ *
+ */
+// if (age > 18) {
+//   document.getElementById("res")[0].innerHTML = "Eligible";
+//     console.log(`${user_input} you are eligible to vote as your age is ${age}`);66
+// } else {
+//   document.getElementById("res")[0].innerHTML = "Not Eligible";
+//   //   console.log(`${user_input} you are not eligible to vote as your age is ${age}`);
+// }
+
+//--------Task-3-----------
+
+// let age = prompt("Enter age:");
+// let price = 200;
+// if (age > 3 && age < 5) {
+//   price -= 100;
+//   console.log(`Half Ticket Price:${price}`);
+// } else {
+//   console.log(`Full Ticket Price:${price}`);
+// }
