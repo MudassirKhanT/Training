4dc21fa632db4b5d9bd5c3be4d45fc348c8632b6
MudassirKhanT <mudassirkhantailor@gmail.com>
2025-04-28 15:21:00 +0530

    strings

---
+++ javascript/strings.js
@@ -0,0 +1,9 @@
+// s = "Welcome to javascript";
+// console.log(s.toUpperCase());
+// console.log(s.toLowerCase());
+// s.replaceAll("W", "Z");
+
+// console.log(s.slice(5, 15));
+
+// name = "mudassir";
+// console.log(`@${name}${name.length}`);
