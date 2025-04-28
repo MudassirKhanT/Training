0bd5482b71e118bdc863c4d1f3e7eb995a8191fd
MudassirKhanT <mudassirkhantailor@gmail.com>
2025-04-28 14:11:06 +0530

    conditional statements in js

--- javascript/conditional.js
+++ javascript/conditional.js
@@ -37,17 +37,17 @@
 // }
 
 //--------------Task-5-----------
-let percentage = prompt("Enter Percentage:");
-if (percentage >= 90 && percentage < 100) {
-  console.log("A");
-} else if (percentage >= 80 && percentage < 90) {
-  console.log("B");
-} else if (percentage >= 70 && percentage < 80) {
-  console.log("C");
-} else if (percentage >= 60 && percentage < 70) {
-  console.log("D");
-} else if (percentage >= 50 && percentage < 60) {
-  console.log("E");
-} else {
-  console.log("F");
-}
+// let percentage = prompt("Enter Percentage:");
+// if (percentage >= 90 && percentage < 100) {
+//   console.log("A");
+// } else if (percentage >= 80 && percentage < 90) {
+//   console.log("B");
+// } else if (percentage >= 70 && percentage < 80) {
+//   console.log("C");
+// } else if (percentage >= 60 && percentage < 70) {
+//   console.log("D");
+// } else if (percentage >= 50 && percentage < 60) {
+//   console.log("E");
+// } else {
+//   console.log("F");
+// }
