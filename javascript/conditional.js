5feeaed74bc014ff6f6723ee6f22305f4986393a
MudassirKhanT <mudassirkhantailor@gmail.com>
2025-04-28 14:12:06 +0530

    conditional statements in js

--- javascript/conditional.js
+++ javascript/conditional.js
@@ -51,3 +51,12 @@
 // } else {
 //   console.log("F");
 // }
+
+// let color = prompt("Enter the color:");
+// if (color == "light") {
+//   console.log("Changed to Dark");
+// } else if (color == "dark") {
+//   console.log("changed to light");
+// } else {
+//   console.log("Invalid color option");
+// }
