60435a2fc153a216df8fd626916cd509755d08e4
MudassirKhanT <mudassirkhantailor@gmail.com>
2025-04-28 19:17:45 +0530

    arrays and its methods in js

---
+++ javascript/arrays.js
@@ -0,0 +1,35 @@
+// let fruits = ["apple", "banana", "orange"];
+// fruits[0] = "watermelon";
+// console.log(fruits);
+// let marks = [1, 2, 3, 4];
+// marks.push(5);
+// console.log(marks);
+// marks.pop();
+// console.log(marks);
+// marks.unshift(10);
+// console.log(marks);
+// marks.shift();
+// console.log(marks);
+// marks.splice(2, 1, 10);
+// console.log(marks);
+let arr = [1, 2, 2, 3, 4, 5, 3, 5, 3, 2, 1];
+//console.log(arr.slice(3, 5));
+// let ans = [];
+// for (let i of arr) {
+//   ans.push((i *= 2));
+// }
+// console.log(ans);
+// ans = ans.sort();
+// console.log(ans[ans.length - 1]);
+// let ans = [];
+// arr.forEach((ele) => {
+//   ans.push(ele * 2);
+// });
+// console.log(ans);
+let ans = [];
+for (let i of arr) {
+  if (ans.indexOf(i) == -1) {
+    ans.push(i);
+  }
+}
+console.log(ans);
