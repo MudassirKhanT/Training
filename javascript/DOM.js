514b0c0707dfac72c2ce4d7df92a47011a223061
MudassirKhanT <mudassirkhantailor@gmail.com>
2025-04-29 19:14:44 +0530

    random color generator and brick building

--- javascript/DOM.js
+++ javascript/DOM.js
@@ -2,9 +2,51 @@
  * Class selectors
  * Id
  * Query
- *query all
+ * query all
  */
 
-let para = document.getElementsByClassName("p1")[1];
-//Returns HTML Collection
-console.log(para);
+// let para = document.getElementsByClassName("p1")[1];
+// //Returns HTML Collection
+// console.log(para);
+// para.style.color = "red";
+// para.style.fontSize = "40px";
+// para.style.background = "yellow";
+// para.innerHTML = "<h1>Welcome to Dom</h1>";
+// let box = document.querySelector(".div")[0];
+// console.log(box);
+// for (let i = 0; i <= 3; i++) {
+//   let myClass = document.getElementsByClassName("div")[i];
+//   myClass.innerText = `Box Number ${i}`;
+//   myClass.style.color = "black";
+//   myClass.style.backgroundColor = "yellow";
+// }
+
+// let i = 99;
+// let para1 = document.getElementsByClassName("p")[0];
+
+// while (i > 0) {
+//   para1.innerText =
+//     para1.innerText +
+//     `${i} bottels of beer on the wall, ${i} bottles of beer.
+//  Take one down and pass it around, ${i - 1} bottles of beer on the wall`;
+//   i -= 1;
+// }
+// let div = document.createElement("div");
+// div.style.height = "100px";
+// div.style.width = "100px";
+// div.style.backgroundColor = "orange";
+// document.body.appendChild(div);
+// div.style.position = "relative";
+// div.style.left = "300px";
+// console.log(div);
+function brick() {
+  let div = document.createElement("div");
+  div.style.height = "50px";
+  div.style.width = "50px";
+  div.style.backgroundColor = "red";
+
+  div.style.border = "1px solid black";
+  document.body.style.display = "flex";
+  document.body.style.flexWrap = "wrap";
+  document.body.appendChild(div);
+}
---
+++ javascript/random.js
@@ -0,0 +1,13 @@
+// function randomColor() {
+//   let blank = "";
+//   let hexCode = "123456789ABCDEF";
+
+//   for (let i = 0; i < 6; i++) {
+//     blank = blank + hexCode[Math.floor(Math.random() * 16)];
+//   }
+//   let color = "#" + blank;
+//   document.body.style.backgroundColor = color;
+//   let btn = document.getElementById("b");
+//   btn.style.backgroundColor = color;
+// }
+// let btn = document.getElementById("b");
