24bfc6fa800fc8d7ec7a0171e12a5b3c992527c4
mudassir <mudassirkhantailor@gmail.com>
2025-05-04 15:06:05 +0530

    Custom Hook

--- react/Training-React/src/UseFetch.jsx
+++ react/Training-React/src/UseFetch.jsx
@@ -1,14 +1,15 @@
 import { useEffect, useState } from "react";
 const UseFetch = (url) => {
-  const [data, setData] = useState([]);
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
   useEffect(() => {
     setTimeout(() => {
       fetch(url)
         .then((res) => res.json())
         .then((data) => setData(data))
-        .catch((err) => console.log(err));
+        .catch((err) => setError(err.message));
     }, 2000);
   }, [url]);
-  return [data, setData];
+  return [data, setData, error];
 };
 export default UseFetch;
