8b80cc12c23ac04900f84b88badf0d6f97124316
mudassir <mudassirkhantailor@gmail.com>
2025-05-04 11:05:25 +0530

    Generated data from backend using setTimeOut Function

--- react/Training-React/src/Books/Bookslist.jsx
+++ react/Training-React/src/Books/Bookslist.jsx
@@ -3,15 +3,25 @@ import { useEffect, useState } from "react";
 // import book2 from "../assets/book2.jpg";
 import Books from "./Books";
 // import book2 from "../../src/assets/book2.jpg";
-
 function Bookslist() {
   const [book, setBook] = useState([]);
+  // const [loading, setLoading] = useState(false);
   useEffect(() => {
-    fetch("http://localhost:3000/books")
-      .then((res) => res.json())
-      .then((data) => setBook(data))
-      .catch((err) => console.log(err));
+    setTimeout(() => {
+      fetch("http://localhost:3000/books")
+        .then((res) => res.json())
+        .then((data) => setBook(data))
+        .catch((err) => console.log(err));
+    }, 2000);
   }, []);
+
+  //------------------Fetching Data From Json data-----------
+  // useEffect(() => {
+  //   fetch("http://localhost:3000/books")
+  //     .then((res) => res.json())
+  //     .then((data) => setBook(data))
+  //     .catch((err) => console.log(err));
+  // }, []);
   // useEffect(() => {
   //   console.log("Use Effect called in booklist");
   // }, [book]);
@@ -25,14 +35,11 @@ function Bookslist() {
     setBook(newBooks);
     // console.log("New:", newBooks);
   }
-
   const listOFBooks = book.map((b, index) => (
     // console.log(b);
     <Books key={index} image={b.image} bookName={b.bookName} paise={b.paise} id={b.id} delete={handleDelete} />
   ));
   console.log(listOFBooks);
-
   return <>{listOFBooks}</>;
 }
-
 export default Bookslist;
