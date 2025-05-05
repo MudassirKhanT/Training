4554e83d5ddc7b20286fe5b45696b196f88eadb6
mudassir <mudassirkhantailor@gmail.com>
2025-05-05 21:51:58 +0530

    added "Refactoring of all projects"

--- javascript/random.js
+++ javascript/random.js
@@ -1,13 +1,12 @@
-// function randomColor() {
-//   let blank = "";
-//   let hexCode = "123456789ABCDEF";
+function randomColor() {
+  let blank = "";
+  let hexCode = "123456789ABCDEF";
 
-//   for (let i = 0; i < 6; i++) {
-//     blank = blank + hexCode[Math.floor(Math.random() * 16)];
-//   }
-//   let color = "#" + blank;
-//   document.body.style.backgroundColor = color;
-//   let btn = document.getElementById("b");
-//   btn.style.backgroundColor = color;
-// }
-// let btn = document.getElementById("b");
+  for (let i = 0; i < 6; i++) {
+    blank = blank + hexCode[Math.floor(Math.random() * 16)];
+  }
+  let color = "#" + blank;
+  document.body.style.backgroundColor = color;
+  let btn = document.getElementById("b");
+  btn.style.backgroundColor = color;
+}
